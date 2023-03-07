5510e5f65db933a89e11843401dfeb82251a291a
Nguyen <nna9220@gmail.com>
2023-03-07 15:05:51 +0700

    Demo2

---
+++ app/src/main/java/vn/iotstar/bai01/MainActivity.java
@@ -0,0 +1,15 @@
+package vn.iotstar.bai01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        //setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/vn/iotstar/bai01/Register.java
@@ -0,0 +1,14 @@
+package vn.iotstar.bai01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
